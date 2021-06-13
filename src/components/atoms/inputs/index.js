33530da4d0c6ce103c034440f263dc7aea0e3460
PrinceSP <princepasombaran@gmail.com>
2021-06-13 14:09:55 +0800

    add border and background styling

--- src/components/atoms/inputs/index.js
+++ src/components/atoms/inputs/index.js
@@ -8,7 +8,11 @@ const Inputs = ({className,title,holder,value,fontF,clr,size,height,width=260}) 
     input:{
       height:43,
       width,
-      padding:'0 8px'
+      padding:'0 8px',
+      background: '#F7FDFF',
+      border: '1.5px solid #CED8F0',
+      boxSizing: 'border-box',
+      borderRadius: '3px'
     }
   }
   return (
